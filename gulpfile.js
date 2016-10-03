17c83cafff8f6f9fddfdc751490bf4859aac1560
lazamar <lazaronijunior@gmail.com>
2016-10-03 18:00:05 +0100

    Edit gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,35 @@
 // List all available tasks
+
+const src = 'src';
+const dest = 'dist';
+const path = require('path');
+
 const organiser = require('gulp-organiser');
 organiser.registerAll('./tasks', {
-	'sass': {
-		'src': './public_src/styles/**/*.scss',
-		'dest': './public/styles/'
-	},
-	'copy-static': {
-		'src': './public_src/**/*',
-		'dest': './public',
-		'map': {
-			'./public_src/js_static/**/*': 'public/js'
-		}
-	},
-	'transpile-react': {
-		'watch': './public_src/js_es6/**/*.js',
-		'src': './public_src/js_es6/*.js',
-		'dest': './public/js',
-    'config': { 'external': ['react', 'react-dom'], exports: 'named', format: 'cjs' },
-	},
-	'link-dependencies': {
-		'dest': './public/js'
-	},
+  sass: {
+    src: path.join(src, 'styles/**/*.scss'),
+    dest: path.join(dest, 'styles'),
+  },
+  'copy-static': {
+    src,
+    dest,
+    map: {
+      [path.join(src, 'js_static/**/*')]: 'public/js',
+    },
+  },
+  'transpile-react': {
+    watch: path.join(src, 'js', '/**/*.js'),
+    src: path.join(src, 'js', 'main.js'),
+    dest: './public/js',
+    config: {
+      external: ['react', 'react-dom'],
+      exports: 'named',
+      format: 'cjs',
+    },
+  },
+  'link-dependencies': {
+    dest: './public/js',
+  },
   'browser-sync': {
     src: '.', // it doesn't matter, it's just so the task object is not ignored.
     reloadOn: ['transpile-react'], // reload page when these tasks happen
@@ -28,10 +37,10 @@ organiser.registerAll('./tasks', {
     baseDir: './',
   },
   'test-browser': {
-    src:'./tests/unit/unit.js',
+    src: './tests/unit/unit.js',
   },
   'test-headless': {
-    src:'./tests/unit/unit.js',
+    src: './tests/unit/unit.js',
   },
   'build-elm': {
     src: 'src/Main.elm',
