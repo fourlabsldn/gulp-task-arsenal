d28275e8f943fb885c93c1865ed2c51d3ba063f3
lazamar <lazaronijunior@gmail.com>
2017-01-11 09:38:07 +0000

    Improve watch implementation

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -3,18 +3,25 @@
 // Select which tasks to watch
 //
 //
-const gulp = require('gulp');
-const organiser = require('gulp-organiser');
+const gulp = require("gulp");
+const organiser = require("gulp-organiser");
 
-module.exports = organiser.register((task, allTasks) => {
+module.exports = organiser.register((task, allTasksConfig) => {
   const { taskNames } = task; // config. Tasks to be watched
+  const allTasks = allTasksConfig
+      .map(t =>
+        t.tasks.length > 1
+          ? t.tasks.concat(t)
+          : t.tasks
+      )
+      .reduce((acc, ts) => acc.concat(ts), []);
 
-  const toWatch = allTasks.filter(t => taskNames.includes(t.tasks[0].name));
+  const toWatch = allTasks.filter(t => taskNames.includes(t.name));
 
   gulp.task(task.name, () => {
     toWatch.forEach(t => {
-      console.log(`watching ${t.tasks[0].name}`);
-      gulp.watch(t.tasks[0].watch || t.tasks[0].src, [t.name]);
+      console.log(`watching ${t.name}`);
+      gulp.watch(t.tasks.watch || t.tasks.src, [t.name]);
     });
   });
 });
